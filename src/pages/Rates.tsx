c91ec96e3098a3e8d97f449306e4bf1a5f8b1f01
Liang Yu Lin <tanyalin2001@gmail.com>
2025-03-30 00:57:12 -0500

    Purchase form

--- src/pages/Rates.tsx
+++ src/pages/Rates.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import states from '../constants/states';
 
 export default function RatesPage() {
   const [formData, setFormData] = useState({
@@ -30,60 +31,6 @@ export default function RatesPage() {
     'w-full border border-gray-300 rounded-md px-4 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-gold';
   const sectionBox = 'bg-white p-6 rounded-xl shadow-md max-w-xl mx-auto space-y-6';
   const labelStyle = 'block text-sm font-semibold text-gray-700 mb-2';
-
-  const states = [
-    '',
-    'Alabama',
-    'Alaska',
-    'Arizona',
-    'Arkansas',
-    'California',
-    'Colorado',
-    'Connecticut',
-    'Delaware',
-    'Florida',
-    'Georgia',
-    'Hawaii',
-    'Idaho',
-    'Illinois',
-    'Indiana',
-    'Iowa',
-    'Kansas',
-    'Kentucky',
-    'Louisiana',
-    'Maine',
-    'Maryland',
-    'Massachusetts',
-    'Michigan',
-    'Minnesota',
-    'Mississippi',
-    'Missouri',
-    'Montana',
-    'Nebraska',
-    'Nevada',
-    'New Hampshire',
-    'New Jersey',
-    'New Mexico',
-    'New York',
-    'North Carolina',
-    'North Dakota',
-    'Ohio',
-    'Oklahoma',
-    'Oregon',
-    'Pennsylvania',
-    'Rhode Island',
-    'South Carolina',
-    'South Dakota',
-    'Tennessee',
-    'Texas',
-    'Utah',
-    'Vermont',
-    'Virginia',
-    'Washington',
-    'West Virginia',
-    'Wisconsin',
-    'Wyoming',
-  ];
 
   return (
     <div>
