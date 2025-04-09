0e38e808a843f224d0aea226ea1a2af434658783
Liang Yu Lin <tanyalin2001@gmail.com>
2025-04-08 23:52:50 -0500

    Add animation to the nav, and fix the styling of the nav

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import ScrollToTop from './components/ScrollToTop';
 import './styles/global.css';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
